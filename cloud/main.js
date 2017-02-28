0871efdc35ad18bcbcf8f67a03cd7d86b2338009
Cyril Saade <csaade@bu.edu>
2017-02-27 20:08:29 -0500

    fixed API call

--- cloud/main.js
+++ cloud/main.js
@@ -311,49 +311,61 @@ Parse.Cloud.afterSave("SensorData", function(req, res) {
     res.success("success")
 });
 
-// Parse.Cloud.beforeSave("Photos", function(req, res) {
+Parse.Cloud.beforeSave("CentralHubData", function(req, res) {
 
-// 	var photoObject = req.object;
+	var photoObject = req.object;
 
-//   //if(!photoObject.existed()) {
+  //if(!photoObject.existed()) {
 
-//     var imageString = photoObject.get("encrypStr");
-//     imageString.replace(/\r?\n|\r/g, "");
+    var imageURL = photoObject.get("photoFile").url;
+    var centralHub = photoObject.get("centralHub");
 
-//     console.log("Predicting...");
-//     app.models.predict(Clarifai.FOOD_MODEL, {base64: imageString}).then(
-//         function(response) {
-//             console.log("Found something!");
-//             //console.log(response.outputs[0]["data"].concepts); // printing all of the detected ingredients from image
-//             Parse.Cloud.run('searchInFoodItem', {"APIresponse": response.outputs[0]["data"].concepts}, {
-//               useMasterKey: true,
-//               success: function(res) {
-//                 console.log("successfully called searchInFoodItem method");
+    var queryToGetUserID = new Parse.Query("CentralHub");
+    queryToGetUserID.equalTo("objectId", photoObject.get("centralHub").id);
 
-//                 Parse.Cloud.run('saveToUsersFoodItem', {"APIresponse": response.outputs[0]["data"].concepts, "userID": photoObject.get("user").id}, {
-//                   useMasterKey: true,
-//                   success: function(res) {
-//                     console.log("Successfully called method saveToUsersFoodItem");
-//                   }, 
-//                   error: function(err) {
-//                     console.log("Error while calling function saveToUsersFoodItem");
-//                   }
-//                 });
+    console.log("Predicting...");
+    app.models.predict(Clarifai.FOOD_MODEL, imageURL).then(
+        function(response) {
+            console.log("Found something!");
+            //console.log(response.outputs[0]["data"].concepts); // printing all of the detected ingredients from image
+            Parse.Cloud.run('searchInFoodItem', {"APIresponse": response.outputs[0]["data"].concepts}, {
+              useMasterKey: true,
+              success: function(res) {
+                console.log("successfully called searchInFoodItem method");
 
-//               },
-//               error: function(err) {
-//                 console.log("err: Parse.Cloud.run");
-//               }
-//             });
+                queryToGetUserID.find({
+                  success: function(centralHubObj) {
+                    console.log("Successfully found Central Hub object from pointer");
+                    Parse.Cloud.run('saveToUsersFoodItem', {"APIresponse": response.outputs[0]["data"].concepts, "userID": centralHubObj[0].get("user").id}, {
+                    useMasterKey: true,
+                    success: function(res) {
+                      console.log("Successfully called method saveToUsersFoodItem");
+                    }, 
+                    error: function(err) {
+                      console.log("Error while calling function saveToUsersFoodItem");
+                    }
+                });
 
-//         },
-//         function(err) {
-//           // there was an error
-//           console.log("Error :(");
-//           console.log(err);
-//         }
-//       );
-//   //}
+                  },
+                  error: function(err) {
+                    console.log("Error - Cannot find Central Hub from pointer");
+                  }
+                });
 
-// 	res.success(); // save image in DB
-// });
+              },
+              error: function(err) {
+                console.log("err: Parse.Cloud.run");
+              }
+            });
+
+        },
+        function(err) {
+          // there was an error
+          console.log("Error :(");
+          console.log(err);
+        }
+      );
+  //}
+
+	res.success(); // save image in DB
+});
