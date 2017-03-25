ae968c0cc28e633e059e04f8303a6ec0d3f6dcba
Cyril Saade <csaade@bu.edu>
2017-03-25 17:33:25 -0400

    refreshing feature

--- cloud/main.js
+++ cloud/main.js
@@ -354,7 +354,7 @@ Parse.Cloud.define("saveToUsersFoodItem", function(req, res) {
           for(var i=0; i<previousUserInventory.length; i++) {
             for(var j=0; j<foodItemsFound.length; j++) {
               console.log("HERE AS WELL :)");
-              if(previousUserInventory[i].foodItem.clarifaiID != foodItemsFound[j].get('clarifaiID'))
+              if(previousUserInventory[i].foodItem['clarifaiID'] != foodItemsFound[j].get('clarifaiID'))
                 num_diff++;
             }
           }
